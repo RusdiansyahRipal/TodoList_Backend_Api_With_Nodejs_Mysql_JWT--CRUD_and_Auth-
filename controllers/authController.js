f41613f9c72cd39e89778fc758c9772d9690b3b9
RusdiansyahRipal <rusdiansyahripal@gmail.com>
2026-06-06 22:26:04 +0700

    membuat code untuk bcrypt password sebelum di-INSERT ke DB. dan ini belum dites. barus buat aja

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,9 @@
 const response = require('../response')
 const db = require('../connection.js')
+const bcrypt = require ('bcrypt');
 
 // 1. REGISTER =========================================================
-const register = (req,res)=>{
+const register = async (req,res)=>{
 
     // ambil data dar req.bod
     const { username, email, password} = req.body
@@ -26,6 +27,10 @@ const register = (req,res)=>{
         if( result.length > 0 ) {
             return response(400,null,'Email Sudah Terdafatar',res)
         }
+
+        // hash password
+        const hashPassword = await bcrypt.hash(password,10)
+        console.log(hashPassword)
 
         // respon jika berhasil
     response (
